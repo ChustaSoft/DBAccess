7e3dc626cf8fe12867f7796fb1dd08d7c79339e0
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-10 12:55:32 +0200

    Automatic configurations for .NET Core projects done

---
+++ ChustaSoft.Tools.DBAccess/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ChustaSoft.Tools.DBAccess.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public static class IServiceCollectionExtensions
+    {
+
+        public static IServiceCollection RegisterDbAccess<TContext>(this IServiceCollection services)
+            where TContext : DbContext
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork<TContext>>();
+
+            return services;
+        }
+
+    }
+}
