bb68b5a2134ac47487d54a02c6874cc619e89e7f
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-10 12:55:16 +0200

    Queryable Extensions created for better reusability

---
+++ ChustaSoft.Tools.DBAccess/IQueryableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public static class IQueryableExtensions
+    {
+
+        public static IQueryable<TEntity> TrySetPagination<TEntity>(this IQueryable<TEntity> query, int? skippedBatches, int? batchSize)
+            where TEntity : class
+        {
+            if (skippedBatches != null && batchSize != null)
+                query = query
+                    .Skip((skippedBatches.Value - 1) * batchSize.Value)
+                    .Take(batchSize.Value);
+
+            return query;
+        }
+
+        public static IQueryable<TEntity> TryIncludeProperties<TEntity>(this IQueryable<TEntity> query, IList<Expression<Func<TEntity, object>>> includedProperties)
+            where TEntity : class
+        {
+            if (includedProperties != null)
+                foreach (var property in includedProperties)
+                    query = query.Include(property);
+
+            return query;
+        }
+
+        public static IQueryable<TEntity> TrySetOrder<TEntity>(this IQueryable<TEntity> query, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy)
+            where TEntity : class
+        {
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return query;
+        }
+
+        public static IQueryable<TEntity> TrySetFilter<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> filter)
+            where TEntity : class
+        {
+            if (filter != null)
+                query = query.Where(filter);
+
+            return query;
+        }
+
+    }
+}
