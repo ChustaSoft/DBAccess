df87110f30d7716b5c807ee1fc70c555c46b2264
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-10 11:19:31 +0200

    Abstractions layer created

---
+++ ChustaSoft.Tools.DBAccess.Abstractions/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using ChustaSoft.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChustaSoft.Tools.DBAccess.Abstractions
+{
+    public interface IRepository<TEntity, TKey> 
+        where TEntity : class, IKeyable<TKey>
+    {
+
+        TEntity GetSingle(TKey id);
+
+        IEnumerable<TEntity> GetMultiple(
+                Expression<Func<TEntity, bool>> filter = null, 
+                Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, 
+                IList<Expression<Func<TEntity, object>>> includeProperties = null
+            );
+
+
+        void Insert(TEntity entity);
+
+        void Insert(IEnumerable<TEntity> entities);
+
+        void Update(TEntity entityToUpdate);
+
+        void Update(List<TEntity> entities);
+
+        void Delete(TKey id);
+
+        void Delete(TEntity entities);
+
+    }
+}
---
+++ ChustaSoft.Tools.DBAccess.Abstractions/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using ChustaSoft.Common.Contracts;
+using System.Threading.Tasks;
+
+namespace ChustaSoft.Tools.DBAccess.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity, TKey> GetRepository<TEntity, TKey>() 
+            where TEntity : class, IKeyable<TKey>;
+
+        bool CommitTransaction();
+
+        Task<bool> CommitTransactionAsync();
+
+    }
+}
