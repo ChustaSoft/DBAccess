79667706309ff632b55ec8886a1c2e57fd73d5ae
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-13 19:11:04 +0200

    Configuration changed, added Unity for .NET Framework automatic DI registration

---
+++ ChustaSoft.Tools.DBAccess.EntityFramework/ConfigurationExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace ChustaSoft.Tools.DBAccess
+{
+
+#if NETCORE
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ConfigurationExtensions
+    {
+
+        public static IServiceCollection RegisterDbAccess<TContext>(this IServiceCollection services)
+            where TContext : DbContext
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork<TContext>>();
+            
+            return services;
+        }
+
+    }
+
+#endif
+
+
+#if NETFRAMEWORK
+
+    using Unity;
+    using System.Data.Entity;
+
+    public static class ConfigurationExtensions
+    {
+
+        public static UnityContainer RegisterDbAccess<TContext>(this UnityContainer container)
+            where TContext : DbContext
+        {
+            container.RegisterType<IUnitOfWork, UnitOfWork<TContext>>();
+
+            return container;
+        }
+
+    }
+
+#endif
+}
