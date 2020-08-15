1cebc039fa9eb69e7b52fac733e5b8237c5bbc3b
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-15 17:27:07 +0200

    Added empty implementation for MongoDb Repositories

---
+++ ChustaSoft.Tools.DBAccess.MongoDb/AsyncRepositoryBase.cs
@@ -0,0 +1,81 @@
+﻿using ChustaSoft.Common.Builders;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public class AsyncRepositoryBase<TEntity, TKey> : IAsyncRepository<TEntity, TKey>
+        where TEntity : class
+    {
+
+        private readonly IMongoContext _context;
+        private readonly IMongoCollection<TEntity> _dbSet;
+
+
+        public AsyncRepositoryBase(IMongoContext mongoContext)
+        {
+            _context = mongoContext;
+            _dbSet = mongoContext.GetCollection<TEntity>();
+        }
+
+
+        public Task<TEntity> GetSingleAsync(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> filter, SelectablePropertiesBuilder<TEntity> includedProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+#if NETCOREAPP3_1
+
+        public IAsyncEnumerable<TEntity> GetMultipleAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            SelectablePropertiesBuilder<TEntity> includedProperties = null,
+            int? skippedBatches = null,
+            int? batchSize = null,
+            bool trackingEnabled = false)
+        {
+            throw new NotImplementedException();
+        }
+
+#endif
+
+#if NETCORE
+
+        public async Task<bool> InsertAsync(TEntity entity)
+        {
+            return await Task.Run(() =>
+            {
+                _context.AddCommand(async () => await _dbSet.InsertOneAsync(entity));
+
+                return true;
+            });
+        }
+
+        public async Task<bool> InsertAsync(IEnumerable<TEntity> entities)
+        {
+            return await Task.Run(() =>
+            {
+                _context.AddCommand(async () => await _dbSet.InsertManyAsync(entities));
+
+                return true;
+            });
+        }
+
+#endif
+
+        
+
+    }
+
+}
---
+++ ChustaSoft.Tools.DBAccess.MongoDb/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using ChustaSoft.Common.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public class RepositoryBase<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : class
+    {
+        public void Delete(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetMultiple(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, SelectablePropertiesBuilder<TEntity> includedProperties = null, int? skippedBatches = null, int? batchSize = null, bool trackingEnabled = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetSingle(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetSingle(Expression<Func<TEntity, bool>> filter, SelectablePropertiesBuilder<TEntity> includedProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
