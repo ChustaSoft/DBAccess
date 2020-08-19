d4e1e302d3908e27ff5c8bd30b0c606ec937806e
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-19 16:46:49 +0200

    Parameter builder specific for Mongo implementation

---
+++ ChustaSoft.Tools.DBAccess.MongoDb/MongoSearchParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace ChustaSoft.Tools.DBAccess
+{
+    public class MongoSearchParameters<TEntity> : SearchParameters<TEntity>
+        where TEntity : class
+    {
+        public MongoSearchParameters()
+            : base()
+        { }
+
+    }
+}
