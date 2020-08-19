8e82c3af19e2d4803e07ed91134f4e545632aeff
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-08-19 16:41:50 +0200

    IQueryableExtensions splitted between EF and Abstractions

---
+++ ChustaSoft.Tools.DBAccess.Abstractions/IQueryableExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public static partial class IQueryableExtensions
+    {
+
+        public static IQueryable<TEntity> TrySetPagination<TEntity>(this IQueryable<TEntity> query, int? batchSize, int? skippedBatches)
+            where TEntity : class
+        {
+            if (skippedBatches != null && batchSize != null)
+                query = query
+                    .Skip((skippedBatches.Value - 1) * batchSize.Value)
+                    .Take(batchSize.Value);
+
+            return query;
+        }
+     
+        public static IQueryable<TEntity> TrySetOrder<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, object>> order, OrderType? orderType)
+           where TEntity : class
+        {
+            if (order != null && orderType != null)
+                switch (orderType)
+                {
+                    case OrderType.Descending:
+                        query = query.OrderByDescending(order);
+                        break;
+                    case OrderType.Ascending:
+                    default:
+                        query = query.OrderBy(order);
+                        break;
+                }
+
+            return query;
+        }
+
+        public static IQueryable<TEntity> TrySetFilter<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> filter)
+            where TEntity : class
+        {
+            if (filter != null)
+                query = query.Where(filter);
+
+            return query;
+        }
+
+    }
+}
--- ChustaSoft.Tools.DBAccess.EntityFramework/IQueryableExtensions.cs
+++ ChustaSoft.Tools.DBAccess.EntityFramework/IQueryableExtensions.cs
@@ -11,19 +11,8 @@ using System.Linq.Expressions;
 
 namespace ChustaSoft.Tools.DBAccess
 {
-    public static class IQueryableExtensions
+    public static partial class IQueryableExtensions
     {
-
-        public static IQueryable<TEntity> TrySetPagination<TEntity>(this IQueryable<TEntity> query, int? batchSize, int? skippedBatches)
-            where TEntity : class
-        {
-            if (skippedBatches != null && batchSize != null)
-                query = query
-                    .Skip((skippedBatches.Value - 1) * batchSize.Value)
-                    .Take(batchSize.Value);
-
-            return query;
-        }
 
         public static IQueryable<TEntity> TryIncludeProperties<TEntity>(this IQueryable<TEntity> query, SelectablePropertiesBuilder<TEntity> includedProperties)
             where TEntity : class
@@ -31,42 +20,6 @@ namespace ChustaSoft.Tools.DBAccess
             if (includedProperties != null)
                 foreach (var includedProperty in includedProperties.GetSelection())
                     query = query.Include(includedProperty.Name);
-
-            return query;
-        }
-
-        public static IQueryable<TEntity> TrySetOrder<TEntity>(this IQueryable<TEntity> query, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy)
-            where TEntity : class
-        {
-            if (orderBy != null)
-                query = orderBy(query);
-
-            return query;
-        }
-
-        public static IQueryable<TEntity> TrySetOrder<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, object>> order, OrderType? orderType)
-           where TEntity : class
-        {
-            if (order != null && orderType != null)
-                switch (orderType)
-                {
-                    case OrderType.Descending:
-                        query = query.OrderByDescending(order);
-                        break;
-                    case OrderType.Ascending:
-                    default:
-                        query = query.OrderBy(order);
-                        break;
-                }
-
-            return query;
-        }
-
-        public static IQueryable<TEntity> TrySetFilter<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> filter)
-            where TEntity : class
-        {
-            if (filter != null)
-                query = query.Where(filter);
 
             return query;
         }
