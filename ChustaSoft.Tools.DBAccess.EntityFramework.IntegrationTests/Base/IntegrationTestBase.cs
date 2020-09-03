b210805d1dfc2768b9ae6360a4ae66e45cfefd69
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-09-03 17:20:22 +0200

    Added IntegrationTest project

---
+++ ChustaSoft.Tools.DBAccess.EntityFramework.IntegrationTests/Base/IntegrationTestBase.cs
@@ -0,0 +1,30 @@
+﻿using ChustaSoft.Tools.DBAccess.EntityFramework.IntegrationTests.Context;
+using System;
+using TestSupport.EfHelpers;
+
+namespace ChustaSoft.Tools.DBAccess.EntityFramework.IntegrationTests.Base
+{
+    public class IntegrationTestBase : IDisposable
+    {
+
+        protected readonly IUnitOfWork _unitOfWork;
+
+
+        public IntegrationTestBase()
+        {
+            var options = SqliteInMemory.CreateOptions<TestContext>();
+            var testContext = new TestContext(options);
+
+            testContext.Database.EnsureCreated();
+
+            _unitOfWork = new UnitOfWork<TestContext>(testContext);
+        }
+
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+
+    }
+}
---
+++ ChustaSoft.Tools.DBAccess.EntityFramework.IntegrationTests/Helpers/MockDataHelper.cs
@@ -0,0 +1,70 @@
+﻿using ChustaSoft.Common.Contracts;
+using ChustaSoft.Tools.DBAccess.Examples.Models;
+using FizzWare.NBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChustaSoft.Tools.DBAccess.EntityFramework.IntegrationTests.Helpers
+{
+    public class MockDataHelper
+    {
+
+        internal static Guid STATIC_COUNTRY_ID => Guid.Parse("da4ae9b5-f65e-4bd2-8645-e6954c913c14");
+        internal static Guid STATIC_CITY_ID => Guid.Parse("6a344614-c90d-4995-9d8e-ff9a9d17aee1");
+        internal static Guid STATIC_PERSON_ID => Guid.Parse("f79210d7-8be3-4795-a45a-5e46162ac6cc");
+
+
+        internal IList<Country> Countries { get; private set; } = new List<Country>();
+        internal IList<City> Cities { get; private set; } = new List<City>();
+        internal IList<Person> Persons { get; private set; } = new List<Person>();
+        internal IList<Address> Addresses { get; private set; } = new List<Address>();
+
+
+        public MockDataHelper()
+        {
+            Countries = Builder<Country>.CreateListOfSize(10)
+                .TheFirst(1).With(x => x.Id = STATIC_COUNTRY_ID)
+                .Build();
+
+            Cities = Builder<City>.CreateListOfSize(10)
+                .TheFirst(1).With(x => x.Id = STATIC_CITY_ID)
+                .All().With(x => x.CountryId = GetRandomId(Countries)).Build();
+
+            Persons = Builder<Person>.CreateListOfSize(2000)
+                .TheFirst(1).With(x => x.Id = STATIC_PERSON_ID)
+                .Build();
+
+            foreach (var person in Persons) 
+            {
+                int sequenceNumber = 1;
+
+                ((List<Address>)Addresses).AddRange(Builder<Address>.CreateListOfSize(GetRandom(1, 5))
+                    .All()
+                        .With(x => x.Id = Guid.NewGuid())
+                        .With(x => x.Sequence == sequenceNumber++)
+                        .With(x => x.CityId = GetRandomId(Cities))
+                        .With(x => x.PersonId = person.Id)
+                    .Build());
+            }        
+        }
+
+
+        private Guid GetRandomId<T>(IList<T> keyableList)
+            where T : IKeyable<Guid>
+        {
+            var randomId = GetRandom(0, keyableList.Count());
+
+            return keyableList[randomId].Id;
+        }
+
+        private int GetRandom(int min, int max) 
+        {
+            Random rnd = new Random();
+            var randomNumber = rnd.Next(min, max);
+            
+            return randomNumber;
+        }
+
+    }
+}
