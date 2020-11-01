b08582d929b44e61835b513be8702481e63bee7e
Kraai Eltjo <eltjo.kraai@cotecna.es>
2020-11-01 19:31:38 +0100

    add KeyResolver for "Id" properties

---
+++ ChustaSoft.Tools.DBAccess.MongoDb/KeyResolvers/IdPropertyKeyResolver.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+
+namespace ChustaSoft.Tools.DBAccess
+{
+    public class IdPropertyKeyResolver : IKeyResolver
+    {
+        public TKey GetKey<TEntity, TKey>(TEntity entity)
+        {
+            dynamic dynamicEntity = entity;
+
+            try
+            {
+                object id = dynamicEntity.Id;
+                return (TKey)id;
+            }
+            catch (RuntimeBinderException exception)
+            {
+
+                var message = $"Type '{entity.GetType()}' does not have an Id property. Please register an implementation of {nameof(IKeyResolver)} that can process this type";
+                throw new InvalidOperationException(message, exception);
+            }
+            catch (InvalidCastException exception)
+            {
+
+                var message = $"Type '{entity.GetType()}' has an Id property, but it is not of the expected type. Please register an implementation of IKeyResolver that can process this type";
+                throw new InvalidOperationException(message, exception);
+            }
+        }
+    }
+}
