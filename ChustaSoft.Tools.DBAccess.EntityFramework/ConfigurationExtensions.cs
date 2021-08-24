ea1bcc3cd404a18bbcced3cd48b283940fa10ddb
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-08-24 15:35:40 +0200

    Fixed wrong precompilation directive for NET 5.0 DBAccess configuration

--- ChustaSoft.Tools.DBAccess.EntityFramework/ConfigurationExtensions.cs
+++ ChustaSoft.Tools.DBAccess.EntityFramework/ConfigurationExtensions.cs
@@ -1,7 +1,7 @@
 ﻿namespace ChustaSoft.Tools.DBAccess
 {
 
-#if NETCORE || NET5
+#if (NETCORE || NET5_0)
 
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
